b171313e6c7d3c6b7d590df965eaa8bcca9d546c
Daniel Gracia <danielgraciac@gmail.com>
2016-04-10 17:13:57 -0500

    creando y deshaciendo boton con jquery

--- js/app.js
+++ js/app.js
@@ -5,8 +5,30 @@
 //$(".advertencia").show("slow");
 
 
-function enviarAdvertencia(){
+/*function enviarAdvertencia(){
 	jQuery(".advertencia").hide().show("slow");
+}
+
+//le pasamos al handler la funcion  sin parentesis
+$(document).ready(enviarAdvertencia);*/
+
+//1 esconder el spoiler
+$(".spoiler span").hide();
+
+//2 agregar el boton 
+$(".spoiler").append("<button>Mostrar Spoiler</button>");
+
+//cuando se presiona el boton
+$(".spoiler button").click(function(){
+	//3.1 mostrar el spoiler
+	$(".spoiler span").show();
+
+	//deshacerse del boton
+	$(this).remove();
+})
+
+function enviarAdvertencia(){
+	jQuery(".spoiler").hide().show("slow");
 }
 
 //le pasamos al handler la funcion  sin parentesis
